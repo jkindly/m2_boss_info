dbb13264d1f9f6da3a892ae9cb1ad6f0ac701b2f
jkindly <kozupa.jakub@gmail.com>
2024-11-14 19:00:21 +0100

    missing bFirstSpawn in regen.cpp

--- source/server/game/regen.cpp
+++ source/server/game/regen.cpp
@@ -1,8 +1,32 @@
 // find:
-/*
+static void regen_spawn(LPREGEN regen, bool bOnce)
+
+// change it to:
+#ifdef BOSS_INFO
+static void regen_spawn(LPREGEN regen, bool bOnce, bool bFirstSpawn = false)
+#else
+static void regen_spawn(LPREGEN regen, bool bOnce)
+#endif
+
+
+// find:
+if (regen->time != 0)
+{
+	regen_spawn(regen, false);
+	
+// change to:
+if (regen->time != 0)
+{
+#ifdef BOSS_INFO
+	regen_spawn(regen, false, true);
+#else
+	regen_spawn(regen, false);
+#endif
+
+// find:
 if (ch && !bOnce)
 	ch->SetRegen(regen);
-*/
+
 // in function: static void regen_spawn(LPREGEN regen, bool bOnce, bool bFirstSpawn = false)
 // and add below:
 #ifdef BOSS_INFO
